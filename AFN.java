617cb3fdbc595e057f2e5a2d75246a9642027a7d
Ernesto Gonzalez Arellano <negrocherne@gmail.com>
2017-03-12 00:38:50 -0600

    Add files via upload

--- AFN.java
+++ AFN.java
@@ -7,14 +7,34 @@ public class AFN{
   public String q[];
   public String s;
   public String f[];
-  public String tablaTranciones[][][];
+  public String t[][][];
 
-  public AFN(String alf, String q, String s, String f){
+  public AFN(String alf, String q, String s, String f,String t[][]){
+    //Inicializar las caracteristicas del AFN
+    //Inicializar alf,q,s,f
+    iniArreglos(alf,this.alf);
+    iniArreglos(q,this.q);
+    this.s = s;
+    iniArreglos(f,this.f);
+
+    //inicializar la tabla de transiciones
 
   }
 
   private void iniArreglos(String cadena,String arreglo[]){
+    //Inicializar alf,q,s,f
     arreglo = cadena.split(",");
+  }
+
+  private void iniTran(String arreglo[][]){
+    //Metodo para inicializar la tabla de transiciones
+    int a = arreglo.length();
+    int b = arreglo[0].length();
+    int x = 0;
+    if(a>b) x = a;
+    else x = b;
+
+    t = new String[a][b][x]
   }
 
 
