8effc41b9b0be2311fac89a4f928a1af357ecf8e
ErnestoGonAr <ergonzalezar@ittepic.edu.mx>
2017-03-12 00:10:03 -0600

    Creacion de clase AFN

---
+++ AFN.java
@@ -0,0 +1,21 @@
+
+
+public class AFN{
+
+  //Caracteristicas del automata
+  public char alf[];
+  public String q[];
+  public String s;
+  public String f[];
+  public String t[][][];
+
+  public AFN(String alf, String q, String s, String f){
+
+  }
+
+  private void iniArreglos(String cadena,String arreglo[]){
+    arreglo = cadena.split(",");
+  }
+
+
+}
