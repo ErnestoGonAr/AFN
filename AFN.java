f2364bd79f83a7720e4e74d9785da03f912e78fc
ErnestoGonAr <ergonzalezar@ittepic.edu.mx>
2017-03-18 02:02:30 -0600

    TDA YA PERRON

--- AFN.java
+++ AFN.java
@@ -70,7 +70,10 @@ public class AFN{
   //puede inicializar s
   public boolean iniS(String s){
     for(int i = 0;i<this.q.length;i++){
-      if(this.q[i].equals(s))return true;
+      if(this.q[i].equals(s)){
+        this.s = s;
+        return true;
+      }
     }
     return false;
   }
@@ -117,33 +120,97 @@ public class AFN{
     return true;
   }
 
+  public boolean validaCadena(String cadena,String estado){
+    //caso base
+    if(cadena.equals("")){
+      if(esFinal(estado))return true;
+      else return false;
+    }
+
+    //Nos posicionamos en el primer caracter
+    char temp = cadena.charAt(0);
+
+    //revisamos si es un simbolo del alfabeto
+    for(int i=0;i<alf.length;i++){
+      if(alf[i].equals(""+temp)){
+        int a = puntoTransAlf(temp);
+        int b = puntoTransQ(estado);
+        System.out.println(""+a+"  "+b+"          "+temp+"   "+estado);
+
+        String temp2 = tablaT[a][b];// A donde voy
+
+        if(temp2.equals("-"))return false;
+        String temp3[] = temp2.substring(1,temp2.length()-1).split(","); //Arreglo de los estados a donde voy
+        for(int j=0;j<temp3.length;j++){
+          if(validaCadena(cadena.substring(1,cadena.length()),temp3[j]))return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private boolean esFinal(String estado){
+    for(int i =0;i<f.length;i++){
+      if(f[i].equals(estado))return true;
+    }
+    return false;
+  }
+
+  private int puntoTransAlf(char x){
+    int a=0;
+    for(a=0;a<alf.length;a++){
+      if(alf[a].equals(""+x))return a;
+    }
+    return -1;
+  }
+
+  private int puntoTransQ(String estado){
+    int a=0;
+    System.out.println(estado);
+    for(a=0;a<q.length;a++){
+
+      if(q[a].equals(""+estado)){
+      return a;
+      }
+    }
+    return -1;
+  }
+
+
   //Pruebas locochonas
   public static void main(String[] args) {
     //Creacion del objeto
     AFN a = new AFN();
 
     //Introduccion de las caracteristicas
-    System.out.println(a.iniQ("{q1,q2}"));
+    System.out.println(a.iniQ("{q1,q2,q3,q4,q5,q6}"));
     System.out.println(a.iniS("q1"));
-    System.out.println(a.inif("{q2}"));
-    System.out.println(a.iniAlf("{a,b}"));
+    System.out.println(a.inif("{q4,q5,q6}"));
+    System.out.println(a.iniAlf("{x,y}"));
 
     //Creacion de la tabla de tranciciones
     a.tablaTran();
-    a.tablaT[0][0]="{q2}";
-    a.tablaT[1][0]="-";
+    //a
+    a.tablaT[0][0]="{q2,q5}";
     a.tablaT[0][1]="-";
-    a.tablaT[1][1]="{q1}";
+    a.tablaT[0][2]="{q4}";
+    a.tablaT[0][3]="{q2}";
+    a.tablaT[0][4]="{q5}";
+    a.tablaT[0][5]="{q6}";
+
+    a.tablaT[1][0]="-";
+    a.tablaT[1][1]="{q3}";
+    a.tablaT[1][2]="{q4}";
+    a.tablaT[1][3]="-";
+    a.tablaT[1][4]="{q6}";
+    a.tablaT[1][5]="{q6}";
+    //b
+    //a.tablaT[1][0]="-"; //q1 - b
+    //a.tablaT[1][1]="-";//q2 - b
 
     a.validaTabla(a.tablaT);
 
-    
-
+    System.out.println(a.validaCadena("xxy",a.s));
   }
-
-
-
-
-
 
 }
